8f61067aec30b63b9611fa9b526fa7398e29ec52
DiegoRossiMafioletti <diego.rossi@ifes.edu.br>
2022-01-31 10:54:25 -0300

    fix jumbo frames processing

--- main.c
+++ main.c
@@ -313,9 +313,8 @@ l2fwd_launch_one_lcore(__rte_unused void *dummy)
 static void
 l2fwd_usage(const char *prgname)
 {
-	printf("%s [EAL options] -- -p PORTMASK [-q NQ]\n"
+	printf("%s [EAL options] -- -p PORTMASK -j [-q NQ]\n"
 	       "  -p PORTMASK: hexadecimal bitmask of ports to configure\n"
-		   "  -j enable jumbo frames processing\n"
 	       "  -q NQ: number of queue (=ports) per lcore (default is 1)\n"
 	       "  -T PERIOD: statistics will be refreshed each PERIOD seconds (0 to disable, 10 default, 86400 maximum)\n"
 	       "  --[no-]mac-updating: Enable or disable MAC addresses updating (enabled by default)\n"
@@ -432,7 +431,6 @@ l2fwd_parse_timer_period(const char *q_arg)
 
 static const char short_options[] =
 	"p:"  /* portmask */
-	"j:"  /* jumbo frames */
 	"q:"  /* number of queues */
 	"T:"  /* timer period */
 	;
@@ -440,8 +438,6 @@ static const char short_options[] =
 #define CMD_LINE_OPT_MAC_UPDATING "mac-updating"
 #define CMD_LINE_OPT_NO_MAC_UPDATING "no-mac-updating"
 #define CMD_LINE_OPT_PORTMAP_CONFIG "portmap"
-
-static uint8_t CMD_LINE_OPT_ENABLE_JUMBO_FRAMES = 0;
 
 enum {
 	/* long options mapped to a short option */
@@ -483,10 +479,6 @@ l2fwd_parse_args(int argc, char **argv)
 				l2fwd_usage(prgname);
 				return -1;
 			}
-			break;
-
-		case 'j':
-			CMD_LINE_OPT_ENABLE_JUMBO_FRAMES = 1;
 			break;
 
 		/* nqueue */
@@ -768,8 +760,7 @@ main(int argc, char **argv)
 		nb_lcores * MEMPOOL_CACHE_SIZE), 8192U);
 
 	/* create the mbuf pool */
-	uint32_t pkt_len = CMD_LINE_OPT_ENABLE_JUMBO_FRAMES ? MAX_JUMBO_PKT_LEN :
-					RTE_MBUF_DEFAULT_BUF_SIZE;
+	pkt_len = MAX_JUMBO_PKT_LEN;		//RTE_MBUF_DEFAULT_BUF_SIZE;
 	l2fwd_pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", nb_mbufs,
 		MEMPOOL_CACHE_SIZE, 0, pkt_len, rte_socket_id());
 	if (l2fwd_pktmbuf_pool == NULL)
